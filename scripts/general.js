3c764472a2c3ba71a54640ba8172f549703bee97
Markus Tiefenbacher <markus.tiefenbacher@gmail.com>
2012-10-03 22:32:27 +0200

    resolve conflict

--- scripts/general.js
+++ scripts/general.js
@@ -1,23 +1,6 @@
 var socket = io.connect('http://localhost:4000');
 
 $(document).ready(function() {
-<<<<<<< HEAD
-  
-  $('#joystick').draggable({
-    containment: 'parent',
-    revert: true,
-    revertDuration: 100,
-    cursor: 'move',
-    drag: function(event, ui) {
-      var movement = controller.move(event.clientX, event.clientY);
-      socket.emit('movement', movement);
-    },
-    stop: function(event, ui) {
-      socket.emit('movement', { front: 0, side: 0 });
-    }
-  });
-  
-=======
 	
 	$('#joystick').draggable({
 		containment: 'parent',
@@ -37,52 +20,10 @@ $(document).ready(function() {
 		socket.emit('movement', { front: 0, left: 0, back: 0, right: 0 });
 	});
 
->>>>>>> Arrow Controls
 });
 
 var controller = {};
 controller.move =  function(x, y) {
-<<<<<<< HEAD
-  var fb = y; // front-back
-  var lr = x; // left-right
-  var tolerance = 10;
-  var movement = {
-    front: 0,
-    side: 0
-  };
-  
-  // front
-  if(fb < 100-tolerance) {
-    movement.front = Math.abs(((((10/(100-tolerance))*fb/10) - 1)*10));
-    movement.front = Math.round(movement.front)/10;
-    movement.front = (movement.front>1) ? 1 : movement.front;
-  } else if(fb > 100+tolerance) {
-    movement.front = ((((10/(100+tolerance))*fb/10) - 1)*10);
-    movement.front = Math.round(movement.front)/10;
-    movement.front = ((movement.front>1) ? 1 : movement.front) * -1;
-  } else {
-    movement.front = 0;
-  }
-  
-  // side
-  if(lr < 100-tolerance) {
-    movement.side = Math.abs(((((10/(100-tolerance))*lr/10) - 1)*10));
-    movement.side = Math.round(movement.side)/10;
-    movement.side = (movement.side>1) ? 1 : movement.side;
-  } else if(lr > 100+tolerance) {
-    movement.side = ((((10/(100+tolerance))*lr/10) - 1)*10);
-    movement.side = Math.round(movement.side)/10;
-    movement.side = ((movement.side>1) ? 1 : movement.side) * -1;
-  } else {
-    movement.side = 0;
-  }
-    
-  return movement;
-};
-
-
-  
-=======
 	var fb = y; // front-back
 	var lr = x; // left-right
 	var tolerance = 10;
@@ -119,4 +60,3 @@ controller.move =  function(x, y) {
 	
 	return movement;
 };
->>>>>>> Arrow Controls
